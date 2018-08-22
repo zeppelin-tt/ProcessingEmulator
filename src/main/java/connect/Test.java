5b4d3950d5e5bef7fa1147d68f744122f32fde9e
zeppelin_tt <it_zeppelin@mail.ru>
2018-08-23 02:54:04 +0300

    почистил немного

--- src/main/java/connect/Test.java
+++ src/main/java/connect/Test.java
@@ -1,20 +1,8 @@
 package connect;
 
-import org.apache.commons.io.IOUtils;
-import utils.Props;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.sql.SQLException;
-
-import static utils.Action.CREATE;
-
-
 public class Test {
-    public static void main(String[] args) throws SQLException, NoSuchFieldException, IOException {
+    public static void main(String[] args)  {
 
-        System.out.println(CREATE.getAction());
     }
 
 
